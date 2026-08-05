57e84610c27fd9484dfa8114fabb20e6d38b7b8e
maggyc741 <magavillon2002@gmail.com>
2026-08-05 19:58:11 +0000

    cambiosssss

--- landing_api/views.py
+++ landing_api/views.py
@@ -9,3 +9,31 @@ import datetime
 class LandingAPI(APIView):
     name = "Landing API"
     collection_name = "landing"  # Actualiza este nombre si tu colección es diferente
+    def get(self, request):
+
+      # Referencia a la colección
+      ref = db.reference(f'{self.collection_name}')
+
+      # get: Obtiene todos los elementos de la col ección
+      data = ref.get()
+
+      # Devuelve un arreglo JSON
+      return Response(data, status=status.HTTP_200_OK)
+
+
+    def post(self, request):
+
+      data = request.data
+
+      # Referencia a la colección
+      ref = db.reference(f'{self.collection_name}')
+
+      current_time  = datetime.now()
+      custom_format = current_time.strftime("%d/%m/%Y, %I:%M:%S %p").lower().replace('am', 'a. m.').replace('pm', 'p. m.')
+      data.update({"timestamp": custom_format })
+
+      # push: Guarda el objeto en la colección
+      new_resource = ref.push(data)
+
+      # Devuelve el id del objeto guardado
+      return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
